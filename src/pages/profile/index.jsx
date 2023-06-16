0a54ec568df0e9602aa6eab4dd8ace665e5d4f9a
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-16 22:38:42 +0700

    add headers dan footer components

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import Footer from "@/components/Footer"
+import Headers from "@/components/Headers"
+import Image from "next/image"
 
-
-const EditProfile = () => {
-
+const Profile = () => {
   return (
     <>
-      {/* <Header/> */}
+      <Headers />
       <div className="bg-profile bg-cover bg-center font-poppins bg-primary p-10">
         <div className="flex lg:px-[5rem] py-5">
           <span className="text-white text-2xl font-bold">User Profile</span>
@@ -13,21 +13,23 @@ const EditProfile = () => {
         <div className="bg-no-repeat bg-cover bg-slate-100 lg:mx-20 rounded-lg p-10 flex flex-col md:flex-row justify-center gap-10">
           <div className="flex flex-col gap-10 items-center md:w-80">
             <div className="rounded-full overflow-hidden bg-blue-600 flex justify-center items-center w-36 h-36 md:w-32 md:h-32 lg:w-52 lg:h-52">
-              <Image src='/coffee.png' width={200} height={200} alt="" className="rounded-full" />
+              <Image
+                src="/coffee.png"
+                width={200}
+                height={200}
+                alt=""
+                className="rounded-full"
+              />
             </div>
             <div className="flex flex-col justify-center items-center m-2">
               <p className="text-xl font-bold">name</p>
               <p>email</p>
             </div>
             <div className="flex flex-col gap-5">
-              <button
-                className="w-48 md:w-32 lg:w-48 hover:bg-gray-400 active:bg-slate-600 active:scale-[.9] duration-300 bg-primary h-16 rounded-3xl"
-              >
+              <button className="w-48 md:w-32 lg:w-48 hover:bg-gray-400 active:bg-slate-600 active:scale-[.9] duration-300 bg-primary h-16 rounded-3xl">
                 Save
               </button>
-              <button
-                className="w-48 md:w-32 lg:w-48 hover:bg-gray-400 active:bg-slate-600 active:scale-[.9] duration-300 bg-secondary h-16 rounded-3xl"
-              >
+              <button className="w-48 md:w-32 lg:w-48 hover:bg-gray-400 active:bg-slate-600 active:scale-[.9] duration-300 bg-secondary h-16 rounded-3xl">
                 Cancel
               </button>
             </div>
@@ -40,14 +42,10 @@ const EditProfile = () => {
               Do you want to save the change?
             </span>
             <div className="flex flex-col gap-5">
-              <button
-                className="w-48 md:w-32 lg:w-48 hover:bg-gray-400 active:bg-slate-600 active:scale-[.9] duration-300 bg-primary h-16 rounded-3xl"
-              >
+              <button className="w-48 md:w-32 lg:w-48 hover:bg-gray-400 active:bg-slate-600 active:scale-[.9] duration-300 bg-primary h-16 rounded-3xl">
                 Save Change
               </button>
-              <button
-                className="w-48 md:w-32 lg:w-48 hover:bg-gray-400 active:bg-slate-600 active:scale-[.9] duration-300 bg-secondary h-16 rounded-3xl"
-              >
+              <button className="w-48 md:w-32 lg:w-48 hover:bg-gray-400 active:bg-slate-600 active:scale-[.9] duration-300 bg-secondary h-16 rounded-3xl">
                 Cancel
               </button>
             </div>
@@ -67,18 +65,14 @@ const EditProfile = () => {
                   <span className="text-[#9F9F9F] text-[18px] font-[500] leading-[30px] ">
                     Email adress :
                   </span>
-                  <span className="text-[18px] font-rubik ">
-                    email
-                  </span>
+                  <span className="text-[18px] font-rubik ">email</span>
                   <hr className="border-[1px] border-black" />
                 </div>
                 <div className="flex flex-col gap-4">
                   <span className="text-[#9F9F9F] text-[18px] font-[500] leading-[30px]">
                     Delivery adress :
                   </span>
-                  <span className="text-[18px] font-rubik ">
-                    address
-                  </span>
+                  <span className="text-[18px] font-rubik ">address</span>
                   <hr className="border-[1px] border-black" />
                 </div>
               </div>
@@ -87,9 +81,7 @@ const EditProfile = () => {
                   <span className="text-[#9F9F9F] text-[18px] font-[500] leading-[30px] ">
                     Mobile number :
                   </span>
-                  <span className="text-[18px] font-rubik ">
-                    phone
-                  </span>
+                  <span className="text-[18px] font-rubik ">phone</span>
                   <hr className="border-[1px] border-black" />
                 </div>
               </div>
@@ -98,32 +90,26 @@ const EditProfile = () => {
               Details
             </span>
             <div className="grid grid-cols-2 gap-10">
-              <div >
+              <div>
                 <div className="flex flex-col gap-4">
                   <span className="text-[#9F9F9F] text-[18px] font-[500] leading-[30px] ">
                     Display Name :
                   </span>
-                  <span className="text-[18px] font-rubik ">
-                    Name
-                  </span>
+                  <span className="text-[18px] font-rubik ">Name</span>
                   <hr className="border-[1px] border-black" />
                 </div>
                 <div className="flex flex-col gap-4">
                   <span className="text-[#9F9F9F] text-[18px] font-[500] leading-[30px] ">
                     First name :
                   </span>
-                  <span className="text-[18px] font-rubik ">
-                    name
-                  </span>
+                  <span className="text-[18px] font-rubik ">name</span>
                   <hr className="border-[1px] border-black" />
                 </div>
                 <div className="flex flex-col gap-4">
                   <span className="text-[#9F9F9F] text-[18px] font-[500] leading-[30px] ">
                     Last name :
                   </span>
-                  <span className="text-[18px] font-rubik ">
-                    name
-                  </span>
+                  <span className="text-[18px] font-rubik ">name</span>
                   <hr className="border-[1px] border-black" />
                 </div>
               </div>
@@ -131,29 +117,33 @@ const EditProfile = () => {
                 <span className="text-[#9F9F9F] text-[18px] font-[500] leading-[30px] ">
                   Mobile number :
                 </span>
-                <span className="text-[18px] font-rubik ">
-                  phone
-                </span>
+                <span className="text-[18px] font-rubik ">phone</span>
                 <hr className="border-[1px] border-black" />
               </div>
             </div>
             <div className="flex flex-col-2 justify-center gap-10 p-10">
               <div className="flex items-center gap-2">
-                <input type="radio" className="text-[#9F9F9F] text-[18px] font-[500] leading-[30px]" />
+                <input
+                  type="radio"
+                  className="text-[#9F9F9F] text-[18px] font-[500] leading-[30px]"
+                />
                 <label htmlFor="">Male</label>
               </div>
 
               <div className="flex items-center gap-2">
-                <input type="radio" className="text-[#9F9F9F] text-[18px] font-[500] leading-[30px]" />
+                <input
+                  type="radio"
+                  className="text-[#9F9F9F] text-[18px] font-[500] leading-[30px]"
+                />
                 <label htmlFor="">Female</label>
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </>
-  );
-};
+  )
+}
 
-export default EditProfile;
+export default Profile
