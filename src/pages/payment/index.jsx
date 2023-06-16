e818e30132d9a262e77bb760cc325e0a2e49d59c
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-16 21:42:58 +0700

    add component header and footer

--- src/pages/payment/index.jsx
+++ src/pages/payment/index.jsx
@@ -1,16 +1,17 @@
-
-import Image from "next/image";
-// import Header from "@/compnents/Header";
-// import Footer from "@/compnents/Footer";
-import { BsCheck, BsFillCreditCardFill, BsBank2, BsCashCoin } from "react-icons/bs";
-
-
-
+import Headers from "@/components/Headers"
+import Footers from "@/components/Footer"
+import Image from "next/image"
+import {
+  BsCheck,
+  BsFillCreditCardFill,
+  BsBank2,
+  BsCashCoin,
+} from "react-icons/bs"
 
 const PaymentAndDeliveryCust = () => {
   return (
     <>
-      {/* <Header /> */}
+      <Headers />
       <div className="bg-payment bg-center bg-cover bg-no-repeat font-rubik">
         <div className="px-28 py-10 flex flex-col gap-5">
           <div className="relative w-96 self-center">
@@ -60,7 +61,7 @@ const PaymentAndDeliveryCust = () => {
                 <div className="flex gap:[20px] md:gap-[33px]">
                   <div>
                     <Image
-                      src='/paymentBg.png'
+                      src="/paymentBg.png"
                       width={90}
                       height={100}
                       className="rounded-lg"
@@ -70,30 +71,20 @@ const PaymentAndDeliveryCust = () => {
                   <div className="flex-1 text-md md:text-xl">
                     <p className="">product</p>
                     <p>quantity</p>
-                    <p>
-                      size
-                    </p>
+                    <p>size</p>
                   </div>
-                  <p className="text-xl flex items-center">
-                    IDR 10000
-                  </p>
+                  <p className="text-xl flex items-center">IDR 10000</p>
                 </div>
-
               </div>
               <hr className="border-[#D0B8A8] mt-[5%]" />
               <div className="flex flex-col gap-3 mt-[5%]">
                 <div className="flex">
                   <div className="grow">SUB TOTAL</div>
-                  <p>
-                    IDR
-                    100000
-                  </p>
+                  <p>IDR 100000</p>
                 </div>
                 <div className="flex">
                   <div className="grow">TAX & FEES</div>
-                  <div>
-                    IDR 100000
-                  </div>
+                  <div>IDR 100000</div>
                 </div>
                 <div className="flex">
                   <div className="grow">SHIPPING</div>
@@ -112,7 +103,10 @@ const PaymentAndDeliveryCust = () => {
               <div>edit</div>
             </div>
             <div className="bg-white w-10/12 md:w-[50%] rounded-lg px-[42px] py-[30px] flex flex-col gap-5">
-              <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, nostrum.</div>
+              <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Pariatur, nostrum.
+              </div>
               <div className="outline outline-1 outline-[#DFD3C3]"></div>
               <div>phone number</div>
             </div>
@@ -121,28 +115,16 @@ const PaymentAndDeliveryCust = () => {
             </div>
             <div className="bg-white w-10/12 md:w-[50%] rounded-lg px-[42px] py-[30px]">
               <div>
-                <div
-                  className="flex items-center gap-[11px]"
-                >
-                  <input
-                    type="radio"
-                    name="radio-1"
-                    className="radio"
-                  />
+                <div className="flex items-center gap-[11px]">
+                  <input type="radio" name="radio-1" className="radio" />
                   <div className="h-10 w-10 bg-[#F47B0A] flex justify-center items-center rounded-lg">
                     <BsFillCreditCardFill size={20} className="text-white" />
                   </div>
                   <div>Card</div>
                 </div>
                 <div className="outline outline-1 outline-black mt-[17px]"></div>
-                <div
-                  className="flex items-center gap-[11px] pt-[17px]"
-                >
-                  <input
-                    type="radio"
-                    name="radio-1"
-                    className="radio"
-                  />
+                <div className="flex items-center gap-[11px] pt-[17px]">
+                  <input type="radio" name="radio-1" className="radio" />
                   <div className="h-10 w-10 bg-[#895537] flex justify-center items-center rounded-lg">
                     <BsBank2 className="text-white" />
                   </div>
@@ -150,14 +132,8 @@ const PaymentAndDeliveryCust = () => {
                 </div>
                 <div className="outline outline-1 outline-black mt-[17px]"></div>
 
-                <div
-                  className="flex items-center gap-[11px] pt-[17px]"
-                >
-                  <input
-                    type="radio"
-                    name="radio-1"
-                    className="radio"
-                  />
+                <div className="flex items-center gap-[11px] pt-[17px]">
+                  <input type="radio" name="radio-1" className="radio" />
                   <div className="h-10 w-10 bg-[#FFBA33] flex justify-center items-center rounded-lg">
                     <BsCashCoin size={20} className="text-white" />
                   </div>
@@ -165,18 +141,15 @@ const PaymentAndDeliveryCust = () => {
                 </div>
               </div>
             </div>
-            <button
-              className="w-10/12 md:w-[50%] bg-secondary h-16 m-5 rounded-lg text-white font-bold text-lg hover:bg-amber-600 active:bg-amber-700 active:scale-[.9] duration-300"
-            >
+            <button className="w-10/12 md:w-[50%] bg-secondary h-16 m-5 rounded-lg text-white font-bold text-lg hover:bg-amber-600 active:bg-amber-700 active:scale-[.9] duration-300">
               Confirm and Pay
             </button>
           </div>
         </div>
       </div>
-      {/* <Footer /> */}
+      <Footers />
     </>
-  );
-};
+  )
+}
 
-
-export default PaymentAndDeliveryCust;
+export default PaymentAndDeliveryCust
