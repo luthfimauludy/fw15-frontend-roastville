4a06ac3b81b48153efb5385e6c665610ddc79d0b
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-16 19:52:29 +0700

    Main adding auth

---
+++ src/pages/auth/forgot-password.jsx
@@ -0,0 +1,116 @@
+import React, { useState } from "react"
+import side_picture from "../../assets/img/picture_auth.jpg"
+import Image from "next/image"
+import logo from "../../assets/img/logo_roastville.png"
+import { Formik } from "formik"
+import * as Yup from "yup"
+import FooterAuth from "@/components/FooterAuth"
+
+function SignUp() {
+  // const [load, setLoad] = React.useState(false);
+  const validationSchema = Yup.object({
+    email: Yup.string().required("Email is empty !"),
+    password: Yup.string().required("Password is empty !"),
+    phoneNumber: Yup.string().required("PhoneNumber is empty !"),
+  })
+
+  return (
+    <>
+      <div className="h-min-screen flex">
+        <aside className="md:flex md:flex-1 md:block hidden">
+          <Image
+            src={side_picture}
+            className="object-cover"
+            alt="picture_side"
+          />
+        </aside>
+        <div className="bg-login md:bg-login_mobile flex flex-col flex-1">
+          <div className="flex h-[75%] flex-col gap-12 items-center p-5">
+            <div className=" flex flex-col justify-center items-center w-full">
+              <div className="flex gap-2 items-center">
+                <div className="h-14 w-14">
+                  <Image src={logo} className="object-cover" alt="logo" />
+                </div>
+                <div className="text-[20px] font-bold ">RoastVille</div>
+              </div>
+              <div className="text-[52px] leading-[78px] font-bold pt-8">
+                Forgot your password?
+              </div>
+              <p>Don’t worry, we got your back!</p>
+            </div>
+            <div className="flex justify-center w-full px-0 md:px-24">
+              <Formik
+                initialValues={{ email: "", password: "", phoneNumber: "" }}
+                validationSchema={validationSchema}
+                // onSubmit={dologin}
+              >
+                {({
+                  values,
+                  errors,
+                  touched,
+                  handleBlur,
+                  handleSubmit,
+                  handleChange,
+                  isSubmitting,
+                }) => {
+                  return (
+                    <form onSubmit={handleSubmit} className="md:w-full w-full">
+                      <div className="flex flex-col gap-4 ">
+                        <div className="form-control">
+                          <input
+                            type="text"
+                            name="email"
+                            id="email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                            placeholder="Enter your email adress to get link"
+                            className="input input-bordered w-full  px-3"
+                          />
+                          {errors.email && touched.email && (
+                            <label htmlFor="email" className="label p-0">
+                              <span className="label-text-alt font-bold text-md text-error">
+                                {errors.email}
+                              </span>
+                            </label>
+                          )}
+                        </div>
+
+                        <div className="w-full pt-4">
+                          {/* {load ? (
+                            <button
+                              type="submit"
+                              className="w-full btn btn-primary normal-case text-white"
+                            >
+                              <span className="loading loading-spinner loading-sm"></span>
+                            </button>
+                          ) : ( */}
+                          <button
+                            type="submit"
+                            className=" w-full btn btn-primary normal-case text-white"
+                          >
+                            Request Link
+                          </button>
+                          {/* )} */}
+                        </div>
+
+                        <div className="md:pt-4 md:block hidden">
+                          <button className="btn btn-primary rounded-xl w-full normal-case text-[20px] font-bold shadow-xl">
+                            Resend Link
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  )
+                }}
+              </Formik>
+            </div>
+          </div>
+          <FooterAuth />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SignUp
---
+++ src/pages/auth/sign-in.jsx
@@ -0,0 +1,196 @@
+import React, { useState } from "react"
+import side_picture from "../../assets/img/picture_auth.jpg"
+import Link from "next/link"
+import { FcGoogle } from "react-icons/fc"
+import { FaEye, FaEyeSlash } from "react-icons/fa"
+import Image from "next/image"
+import logo from "../../assets/img/logo_roastville.png"
+import { Formik } from "formik"
+import * as Yup from "yup"
+import FooterAuth from "@/components/FooterAuth"
+
+function SignIn() {
+  const [openEye, setOpenEye] = useState(false)
+  // const [load, setLoad] = React.useState(false);
+  const validationSchema = Yup.object({
+    email: Yup.string().required("Email is empty !"),
+    password: Yup.string().required("Password is empty !"),
+  })
+
+  function showEye() {
+    setOpenEye(!openEye)
+  }
+  return (
+    <>
+      <div className="h-min-screen flex">
+        <aside className="md:flex md:flex-1 md:block hidden">
+          <Image
+            src={side_picture}
+            className="object-cover"
+            alt="picture_side"
+          />
+        </aside>
+        <div className="bg-login md:bg-login_mobile flex flex-col flex-1">
+          <div className="flex h-[75%] flex-col gap-12 items-center p-5">
+            <div className=" flex justify-between w-full">
+              <div className="flex gap-2 items-center">
+                <div className="h-14 w-14">
+                  <Image src={logo} className="object-cover" alt="logo" />
+                </div>
+                <div className="text-[20px] font-bold ">RoastVille</div>
+              </div>
+              <div className="flex items-center">
+                <span className="text-[20px] font-bold">Login</span>
+              </div>
+            </div>
+            <div className="flex justify-center w-full px-0 md:px-24">
+              <Formik
+                initialValues={{ email: "", password: "" }}
+                validationSchema={validationSchema}
+                // onSubmit={dologin}
+              >
+                {({
+                  values,
+                  errors,
+                  touched,
+                  handleBlur,
+                  handleSubmit,
+                  handleChange,
+                  isSubmitting,
+                }) => {
+                  return (
+                    <form onSubmit={handleSubmit} className="md:w-full w-full">
+                      <div className="flex flex-col gap-4 ">
+                        <div className="form-control">
+                          <label className="label">
+                            <span className="label-text text-[20px] font-bold">
+                              Email Adress :
+                            </span>
+                          </label>
+                          <input
+                            type="text"
+                            name="email"
+                            id="email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                            placeholder="Enter your email adress"
+                            className="input input-bordered w-full  px-3"
+                          />
+                          {errors.email && touched.email && (
+                            <label htmlFor="email" className="label p-0">
+                              <span className="label-text-alt font-bold text-md text-error">
+                                {errors.email}
+                              </span>
+                            </label>
+                          )}
+                          <label className="label"></label>
+                        </div>
+                        <div className="form-control w-full relative">
+                          <label className="label">
+                            <span className="label-text text-[20px] font-bold">
+                              Password :
+                            </span>
+                          </label>
+                          <input
+                            type={openEye ? "text" : "password"}
+                            name="password"
+                            id="password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                            placeholder="Enter your password"
+                            className="input input-bordered w-full  px-3"
+                          />
+                          <button type="button" onClick={showEye}>
+                            {openEye ? (
+                              <FaEye
+                                size={25}
+                                className="absolute top-12 right-4"
+                              />
+                            ) : (
+                              <FaEyeSlash
+                                size={25}
+                                className="absolute top-12 right-4"
+                              />
+                            )}
+                          </button>
+                          {errors.password && touched.password && (
+                            <label htmlFor="password" className="label p-0">
+                              <span className="label-text-alt font-bold text-md text-error">
+                                {errors.password}
+                              </span>
+                            </label>
+                          )}
+                          <Link href="/auth/forgot-password">
+                            <label className="label">
+                              <p className="text-primary font-bold">
+                                Forgot Password
+                              </p>
+                            </label>
+                          </Link>
+                        </div>
+                        <div className="w-full pt-4">
+                          {/* {load ? (
+                            <button
+                              type="submit"
+                              className="w-full btn btn-primary normal-case text-white"
+                            >
+                              <span className="loading loading-spinner loading-sm"></span>
+                            </button>
+                          ) : ( */}
+                          <button
+                            type="submit"
+                            className=" w-full btn btn-primary normal-case text-white"
+                          >
+                            Sign In
+                          </button>
+                          {/* )} */}
+                        </div>
+                        <div className="btn bg-white mt-4 md:block hidden md:flex gap-4 flex items-center justify-center shadow-2xl normal-case ">
+                          <div>
+                            <FcGoogle size={25} />
+                          </div>
+                          <div className="text-[17px] font-bold ">
+                            Login with Google
+                          </div>
+                        </div>
+                        <div className="flex w-full items-center pt-4">
+                          <div className="border-b-2 w-[50%] "></div>
+                          <div className="text-[#9F9F9F] w-full text-[16px] text-center ">
+                            Don’t have an account?
+                          </div>
+                          <div className="border-b-2 w-[50%]"></div>
+                        </div>
+                        <div className="md:pt-4 md:block hidden">
+                          <Link href="/auth/sign-up">
+                            <button className="btn btn-primary rounded-xl w-full normal-case text-[20px] font-bold shadow-xl">
+                              Sign Up
+                            </button>
+                          </Link>
+                        </div>
+                        <button className="md:hidden btn btn-primary rounded-xl w-full normal-case text-[20px] font-bold">
+                          <div className="flex gap-4 justify-center shadow-bottom-xl">
+                            <div>
+                              <FcGoogle size={25} />
+                            </div>
+                            <div className="text-[17px] font-bold ">
+                              Login with Google
+                            </div>
+                          </div>
+                        </button>
+                      </div>
+                    </form>
+                  )
+                }}
+              </Formik>
+            </div>
+          </div>
+          <FooterAuth />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SignIn
