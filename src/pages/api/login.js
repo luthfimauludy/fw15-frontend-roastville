98b47bcf810f579b97587447a517dca4bb94e48f
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-19 00:29:36 +0700

    fix

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -2,18 +2,25 @@ import { withIronSessionApiRoute } from "iron-session/next"
 import cookieConfig from "@/helpers/cookieConfig"
 
 export default withIronSessionApiRoute(async function loginRoute(req, res) {
-  const request = await fetch("http://localhost:8080/auth/login", {
-    method: "POST",
-    body: new URLSearchParams(req.body).toString(),
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  })
-  const response = await request.json()
-  const token = response?.results?.token
-  if (token) {
-    req.session.token = token
-    await req.session.save()
+  try {
+    const request = await fetch("http://localhost:8080/auth/login", {
+      method: "POST",
+      body: new URLSearchParams(req.body).toString(),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+    const response = await request.json()
+    const token = response?.results?.token
+    if (token) {
+      req.session.token = token
+      await req.session.save()
+    }
+
+    return res.json(response)
+  } catch (err) {
+    const message = err.response?.data?.message
+    if (message) {
+    }
   }
-  return res.json(response)
 }, cookieConfig)
