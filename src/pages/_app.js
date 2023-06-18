e1b61fa686b66c8d01f0ab81cef87bd8778ad2a0
fahmipraga <itmamul2004@gmail.com>
2023-06-18 16:16:39 +0700

    fix: app js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import "@/styles/globals.css"
 import { Rubik } from "next/font/google"
 import { Provider } from "react-redux"
-import store from "../redux/store"
+import { store } from "../redux/store"
 
 const rubik = Rubik({
   weight: "400",
