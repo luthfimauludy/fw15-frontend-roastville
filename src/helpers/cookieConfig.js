6542bc37dcabb9f74333b4c38b7f641af7af4e21
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-18 11:30:55 +0700

    add cookieconfig

---
+++ src/helpers/cookieConfig.js
@@ -0,0 +1,8 @@
+const cookieConfig = {
+  cookieName: "roastville",
+  password: "cmX56pNL%0W&HB8cUzVux9lR&23*a224r",
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+  },
+}
+export default cookieConfig
