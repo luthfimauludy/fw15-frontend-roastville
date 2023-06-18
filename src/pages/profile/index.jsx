9aa7baa532e79e09c03aae3e66989a706282837a
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-18 11:32:15 +0700

    add token user

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -2,7 +2,33 @@ import Footer from "@/components/Footer"
 import Headers from "@/components/Headers"
 import Image from "next/image"
 
-const Profile = () => {
+import checkCredentials from "@/helpers/checkCredentials"
+import cookieConfig from "@/helpers/cookieConfig"
+import { withIronSessionSsr } from "iron-session/next"
+import React from "react"
+import axios from "axios"
+
+export const getServerSideProps = withIronSessionSsr(
+  async function getServerSideProps({ req, res }) {
+    const token = req.session?.token
+    checkCredentials(token, res, "/auth/login")
+    const { data } = await axios.get("http://localhost:8080/profile", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    return {
+      props: {
+        token,
+        user: data.results,
+      },
+    }
+  },
+  cookieConfig
+)
+
+const Profile = ({ token }) => {
   return (
     <>
       <div className="header pb-24">
