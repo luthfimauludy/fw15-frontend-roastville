1927e9cffd335647b14d1aabfbf8b57e0ca9b8fd
fahmipraga <itmamul2004@gmail.com>
2023-07-19 23:38:35 +0700

    fix: responsive footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
   return (
     <>
       <div className="py-14 bg-gray-200">
-        <div className="w-full flex justify-around">
+        <div className="w-full flex justify-around flex-wrap">
           <div className="max-w-lg flex flex-col gap-8">
             <div className="flex items-center gap-2">
               <div className=" h-24 w-24">
