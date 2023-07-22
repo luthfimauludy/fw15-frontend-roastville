80230d6e0ad203c912e52f1a196bdf804978e386
fahmipraga <itmamul2004@gmail.com>
2023-07-22 23:42:54 +0700

    fix: history customer

--- src/pages/product/history-cust.jsx
+++ src/pages/product/history-cust.jsx
@@ -18,21 +18,20 @@ export const getServerSideProps = withIronSessionSsr(async ({ req, res }) => {
 }, cookieConfig)
 
 const HistoryCust = ({ token }) => {
-  const [history, setHistory] = useState([])
+  const [transactionsHistory, setTransactionHistory] = useState([])
   const [errMsg, setErrMsg] = useState("")
 
   useEffect(() => {
-    async function getHistory() {
+    async function getTranscationHistory() {
       try {
-        const { data } = await http(token).get("/history")
-        setHistory(data.results)
+        const { data } = await http(token).get("/transactions/manage")
+        setTransactionHistory(data.results)
       } catch (err) {
-        const message = err.response?.data?.message
-        setErrMsg(message)
+        console.log(err)
       }
     }
 
-    getHistory()
+    getTranscationHistory()
   }, [token])
 
   return (
@@ -47,36 +46,29 @@ const HistoryCust = ({ token }) => {
         </div>
         <div className="flex justify-center items-center pt-20">
           <div className="grid grid-cols-3 gap-5">
-            {history.map((h) => {
-              console.log(h)
-              return (
-                <>
-                  <div className="w-[394px] h-[126px] bg-white rounded-2xl hover:opacity-50 cursor-pointer">
-                    <div className="py-5 px-5 flex gap-5">
-                      <Image
-                        src={h.picture}
-                        width="82"
-                        height="90"
-                        alt="tomato"
-                        className="rounded-full"
-                      />
-                      <div className="flex-1 text-lg">
-                        <p className="font-bold text-2xl">{h.name}</p>
-                        <p>IDR {h.price}</p>
-                        <p>Delivered</p>
-                      </div>
-                      <input
-                        className="self-end"
-                        type="checkbox"
-                        name="check"
-                        id=""
-                      />
-                    </div>
-                    <div></div>
-                  </div>
-                </>
-              )
-            })}
+            <div className="w-[394px] h-[126px] bg-white rounded-2xl hover:opacity-50 cursor-pointer">
+              <div className="py-5 px-5 flex gap-5">
+                {/* <Image
+                  src={h.picture}
+                  width="82"
+                  height="90"
+                  alt="tomato"
+                  className="rounded-full"
+                /> */}
+                <div className="flex-1 text-lg">
+                  <p className="font-bold text-2xl"></p>
+                  <p>IDR </p>
+                  <p>Delivered</p>
+                </div>
+                <input
+                  className="self-end"
+                  type="checkbox"
+                  name="check"
+                  id=""
+                />
+              </div>
+              <div></div>
+            </div>
           </div>
         </div>
       </div>
