66e2e74af7ede874fe0eb1fadd3088fbe4d8b0f2
fahmipraga <itmamul2004@gmail.com>
2023-07-23 21:42:44 +0700

    fix: dispatch product on component did mount

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -7,13 +7,16 @@ import image3 from "/public/img-coupon3.png"
 import Header from "@/components/header"
 import Footer from "@/components/footer"
 import http from "@/helpers/http"
-import { useRouter } from "next/router"
-import { useDispatch } from "react-redux"
-import { productDetail } from "@/redux/reducers/product"
-import { variantDetail } from "@/redux/reducers/variant"
-import { withIronSessionSsr } from "iron-session/next"
 import checkCredentials from "@/helpers/checkCredentials"
 import cookieConfig from "@/helpers/cookieConfig"
+import { useRouter } from "next/router"
+import { useDispatch } from "react-redux"
+import {
+  clearProduct,
+  productDetail,
+  variantDetail,
+} from "@/redux/reducers/product"
+import { withIronSessionSsr } from "iron-session/next"
 
 export const getServerSideProps = withIronSessionSsr(async ({ req, res }) => {
   const token = req.session.token || null
@@ -26,69 +29,50 @@ export const getServerSideProps = withIronSessionSsr(async ({ req, res }) => {
 }, cookieConfig)
 
 function ProductCust({ token }) {
+  const [product, setProduct] = React.useState([])
   const dispatch = useDispatch()
   const router = useRouter()
-  const [product, setProduct] = React.useState([])
 
   const getProduct = React.useCallback(async () => {
     try {
       const { data } = await http().get("/products")
-      console.log(data)
       setProduct(data.results)
+      console.log(data)
     } catch (error) {
-      if (error.isAxiosError && !error.response) {
-        console.log("Axios error")
-      }
+      console.log(error)
     }
   }, [setProduct])
 
   React.useEffect(() => {
     getProduct()
-  }, [getProduct])
+    dispatch(clearProduct())
+  }, [getProduct, dispatch])
 
   const dispatchEvent = (item) => {
     const encodedProductName = encodeURIComponent(item.name)
     const url = `/product/${encodedProductName}`
-    dispatch(
-      productDetail({
-        id: item.id,
-        name: item.name,
-        picture: item.picture,
-        description: item.description,
-      })
-    )
-    dispatch(
-      variantDetail({
-        code: item.variant[0].code,
-        name: item.variant[0].name,
-        price: item.variant[0].price,
-        quantity: item.variant[0].quantity,
-        selectedQty: 1,
-      })
-    )
+    dispatch(productDetail(item))
     router.replace(url)
   }
 
   return (
     <div className="h-min-screen">
-      <div className="pb-24 header">
-        <Header token={token} />
-      </div>
-      <div className="flex">
-        <div className="w-[425px] border-r-2 px-20 py-7">
+      <Header token={token} />
+      <div className="flex flex-wrap-reverse xl:flex-nowrap">
+        <div className="w-full xl:w-[425px] border-r-2 px-20 py-7">
           <div className="flex flex-col text-center mb-16">
             <div className="text-primary font-bold text-2xl">Promo Today</div>
             <div className="text-sm text-center">
               Coupons will be updated every weeks.
               <br />
-              Check them out!{" "}
+              Check them out!
             </div>
           </div>
           <div className="flex flex-col justify-center items-center gap-12">
-            <div className="flex flex-col justify-center items-center gap-5">
+            <div className="flex flex-wrap xl:flex-col justify-center items-center gap-5">
               <div className="flex gap-2 w-80 h-28 border rounded-xl items-center px-3 bg-[#88B788]">
                 <div className="w-[25%]">
-                  {/* <Image src={image} alt="img-coupon.png" /> */}
+                  <Image src={image} alt="img-coupon.png" />
                 </div>
                 <div className="w-[75%]">
                   <div className="font-bold">HAPPY MOTHER&apos;S DAY!</div>
@@ -97,7 +81,7 @@ function ProductCust({ token }) {
               </div>
               <div className="flex gap-2 w-80 h-28 border rounded-xl items-center px-3 bg-[#F5C361]">
                 <div className="w-[25%]">
-                  {/* <Image src={image2} alt="img-coupon2.png" /> */}
+                  <Image src={image2} alt="img-coupon2.png" />
                 </div>
                 <div className="w-[75%]">
                   <div className="font-bold">
@@ -108,7 +92,7 @@ function ProductCust({ token }) {
               </div>
               <div className="flex gap-2 w-80 h-28 border rounded-xl items-center px-3 bg-[#88B788]">
                 <div className="w-[25%]">
-                  {/* <Image src={image} alt="img-coupon.png" /> */}
+                  <Image src={image} alt="img-coupon.png" />
                 </div>
                 <div className="w-[75%]">
                   <div className="font-bold">HAPPY MOTHER&apos;S DAY!</div>
@@ -117,7 +101,7 @@ function ProductCust({ token }) {
               </div>
               <div className="flex gap-2 w-80 h-28 border rounded-xl items-center p-2 bg-[#C59378]">
                 <div className="w-[25%]">
-                  {/* <Image src={image3} alt="img-coupon3.png" /> */}
+                  <Image src={image3} alt="img-coupon3.png" />
                 </div>
                 <div className="w-[75%]">
                   <div className="font-bold">HAPPY HALLOWEEN!</div>
@@ -144,7 +128,7 @@ function ProductCust({ token }) {
                 Create Product
               </div>
             </div>
-            <div className="w-80 text-xs mt-20">
+            <div className="w-80 text-xs">
               <div className="font-bold">Terms and Condition</div>
               <div>
                 1. You can only apply 1 coupon per day
@@ -158,11 +142,11 @@ function ProductCust({ token }) {
             </div>
           </div>
         </div>
-        <div className="w-full flex items-center justify-center">
+        <div className="w-full flex pt-10 justify-center">
           <div className="flex flex-col gap-20">
-            <div className="flex justify-center items-center py-7 gap-11 px-28 cursos-pointer">
+            <div className="flex flex-wrap justify-center gap-10 items-center cursor-pointer">
               <div className="flex text-[#9F9F9F] text-xl justify-center w-48 hover:border-b-2 hover:border-primary duration-100 cursor-pointer hover:shadow-lg hover:scale-[1.05] hover:text-primary">
-                Favorite & Promo
+                Favourite & Promo
               </div>
               <div className="flex justify-center hover:border-b-2 hover:border-primary duration-100 cursor-pointer hover:scale-[1.05]">
                 <div className="text-[#9F9F9F] text-xl hover:text-primary">
@@ -185,44 +169,34 @@ function ProductCust({ token }) {
                 </div>
               </div>
             </div>
-            <div className="grid grid-cols-4 gap-x-24 gap-y-12 pl-4">
-              {product.map((item) => (
-                <div
-                  onClick={() => dispatchEvent(item)}
-                  key={`product-${item.id}`}
-                  className="flex flex-col justify-between bordered-2 items-center w-48 h-56 border border-none rounded-xl shadow-xl p-3 mb-7 cursor-pointer hover:scale-[1.05] active:scale-[.9] duration-300"
-                >
-                  <div className="flex flex-col gap-5 items-center justify-center w-full h-full">
-                    <div className="w-32 justify-center h-32 shadow-lg border rounded-full overflow-hidden object-cover flex items-center mt-[-50px]">
-                      {/* {item.picture === null ? (
-                        <div className="flex justify-center items-center">
-                          <Image
-                            src={default_picture}
-                            alt="img-product.png"
-                            width="400"
-                            height="400"
-                            className="object-cover w-full h-full bg-cover flex justify-center self-center"
-                          />
-                        </div>
-                      ) : (
+            <div className="flex justify-center items-center flex-wrap px-10 gap-10">
+              {product.map((item) => {
+                return (
+                  <div
+                    onClick={() => dispatchEvent(item)}
+                    key={`product-${item.id}`}
+                    className="flex flex-col justify-between bordered-2 items-center w-48 h-56 border border-none rounded-xl shadow-xl p-3 mb-7 cursor-pointer hover:scale-[1.05] active:scale-[.9] duration-300"
+                  >
+                    <div className="flex flex-col gap-5 items-center justify-center w-full h-full">
+                      <div className="w-32 justify-center h-32 shadow-lg border rounded-full overflow-hidden object-cover flex items-center mt-[-50px]">
                         <Image
+                          src={item.picture}
                           alt="img-product.png"
                           width="400"
                           height="400"
-                          src={item.picture}
-                          className="object-cover w-full h-full bg-cover"
+                          className="object-cover w-full h-full bg-cover flex justify-center self-center"
                         />
-                      )} */}
+                      </div>
+                      <div className="text-center font-black text-xl text-accent">
+                        {item?.name}
+                      </div>
                     </div>
-                    <div className="text-center font-black text-xl text-accent">
-                      {item?.name}
+                    <div className="font-bold text-primary">
+                      Rp.{item?.variant[0]?.price}
                     </div>
                   </div>
-                  <div className="font-bold text-primary">
-                    Rp.{item?.variant[0]?.price}
-                  </div>
-                </div>
-              ))}
+                )
+              })}
             </div>
           </div>
         </div>
